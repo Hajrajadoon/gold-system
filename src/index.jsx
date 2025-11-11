7bfb74db2ff83ae559e2897206ce9e2f3e35d32a
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-11-11 18:43:41 +0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Tailwind
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 
