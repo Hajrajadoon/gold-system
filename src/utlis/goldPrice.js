87413994eeaf4e9176bb2898a20b9ede2b3fa24d
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-11-13 18:52:53 +0500

    Update goldPrice.js

--- src/utlis/goldPrice.js
+++ src/utlis/goldPrice.js
@@ -1,4 +1,4 @@
-export async function getGoldPrice() {
+export async function getgoldPrice() {
   try {
     const res = await fetch("https://api.metals.live/v1/spot");
     const data = await res.json();
