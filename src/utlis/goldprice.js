1ec41ee7cadeace02791f2d2e9efa0dc4716a0f9
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-11-13 16:37:51 +0500

    Update goldprice.js

--- src/utlis/goldprice.js
+++ src/utlis/goldprice.js
@@ -2,7 +2,6 @@ export async function getGoldPrice() {
   try {
     const res = await fetch("https://api.metals.live/v1/spot");
     const data = await res.json();
-    // first element = gold price in USD/oz
     return data[0].gold;
   } catch {
     return null;
