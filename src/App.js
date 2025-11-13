289808fd38497c8e9efcf81448cdf88712a5d3e3
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-11-13 19:37:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+// src/App.js
 import React from 'react';
-import Navbar from './shared/Navbar';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
-      <Navbar />
+    <div>
       <Home />
     </div>
   );
