ed6acb022b0cb2ccea16ec777a9c11e1f5830579
Hajrajadoon <48354586+Hajrajadoon@users.noreply.github.com>
2025-11-13 15:54:26 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GoldChain Demo</h1>
+      <p>Welcome to the GoldChain platform.</p>
+    </div>
+  );
+}
+
+export default App;
